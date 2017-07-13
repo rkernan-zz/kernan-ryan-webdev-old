98ce1c64b72fad62bdbbbc3b72f69787784953ff
System Administrator <root@Ryans-MacBook-Pro-2.local>
2017-07-12 23:54:23 -0400

    heroku info update

--- test/app.js
+++ test/app.js
@@ -6,13 +6,21 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
+
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds117889.mlab.com:17889/heroku_plm903jw'; // user yours
+}
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
